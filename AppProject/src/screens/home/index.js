d7dd9028d03af2f218038e2ad23e882c64be1f97
micha <infernogrim@gmail.com>
2022-05-17 11:39:14 -0500

    fixed expo error

---
+++ AppProject/src/screens/home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import {View, Text} from "react-native";
+
+const HomeScreen = (props) => {
+ return (
+  <View>
+   <Text>Hello</Text> 
+  </View>
+ );
+};
+
+export default HomeScreen;
